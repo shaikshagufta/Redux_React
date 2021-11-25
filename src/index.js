fefbe7c898a3063d5c8bb6f17b91f0c45e68be28
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-25 13:54:04 +0530

    login, logout

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
+import { configureStore } from '@reduxjs/toolkit';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import userReducer from "./features/User"
 
+const store = configureStore({
+  reducer: {
+    user: userReducer,
+  },
+});
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store= {store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
